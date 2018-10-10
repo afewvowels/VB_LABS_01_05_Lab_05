c6ebc5577903e818d62234c0702ffa6e6baa1f3b
afewvowels <keithbsmith87@gmail.com>
2018-10-10 13:46:16 -0400

    update

--- Lab_05/SalarySurvey.vb
+++ Lab_05/SalarySurvey.vb
@@ -3,66 +3,74 @@
 'Date: Fall 2014
 'Description: Description of the form being created 
 
+'Date: 10 October 2018
+'Student: Keith Smith
+
+Option Explicit On
+Option Strict On
+
 Public Class SalarySurvey
+    ' Create persistent data array to hold total sale counts
     Dim SalesRangeIntegerArray(8) As Integer
 
     Private Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+        ' Create temporary integer to hold calculated sale value
+        Dim WeeklySaleAmountDecimal As Decimal
         Dim IndividualSaleDecimal As Decimal
+        Dim TempArrayIndexInteger As Integer
+        ' Constant values for base salary and commission rate on sales
+        Const BASE_SALARY_DECIMAL As Decimal = 200D
+        Const COMMISSION_RATE_DECIMAL As Decimal = 0.1D
 
+        ' Try block to catch bad data entered into textfield
         Try
-            IndividualSaleDecimal = 200D + (Convert.ToInt32(amountTextBox.Text) * 0.1D)
+            WeeklySaleAmountDecimal = Convert.ToInt32(amountTextBox.Text)
 
+            ' If number converted is less than 0, throw error (no negative sales value)
+            If (WeeklySaleAmountDecimal < 0) Then
+                Throw New Exception
+            End If
+
+            ' Calculate Individual sale (10% of total sales + $200)
+            IndividualSaleDecimal = BASE_SALARY_DECIMAL + (WeeklySaleAmountDecimal * COMMISSION_RATE_DECIMAL)
+
+            ' Use select case to determine index to pass to array updater below
             Select Case IndividualSaleDecimal
                 Case 200 To 299
-                    SalesRangeIntegerArray(0) += 1
+                    TempArrayIndexInteger = 0
                 Case 300 To 399
-                    SalesRangeIntegerArray(1) += 1
+                    TempArrayIndexInteger = 1
                 Case 400 To 499
-                    SalesRangeIntegerArray(2) += 1
+                    TempArrayIndexInteger = 2
                 Case 500 To 599
-                    SalesRangeIntegerArray(3) += 1
+                    TempArrayIndexInteger = 3
                 Case 600 To 699
-                    SalesRangeIntegerArray(4) += 1
+                    TempArrayIndexInteger = 4
                 Case 700 To 799
-                    SalesRangeIntegerArray(5) += 1
+                    TempArrayIndexInteger = 5
                 Case 800 To 899
-                    SalesRangeIntegerArray(6) += 1
+                    TempArrayIndexInteger = 6
                 Case 900 To 999
-                    SalesRangeIntegerArray(7) += 1
+                    TempArrayIndexInteger = 7
                 Case Else
-                    SalesRangeIntegerArray(8) += 1
+                    TempArrayIndexInteger = 8
             End Select
+
+            ' Update array counter (increment) at calculated array index
+            SalesRangeIntegerArray(TempArrayIndexInteger) += 1
+
         Catch ex As Exception
+            ' Display error message
             MessageBox.Show("Sale data entry error",
                             "Data Entry Error",
                             MessageBoxButtons.OK,
                             MessageBoxIcon.Exclamation)
         End Try
 
-        rangesLabel.Text = String.Format("{0}{1}{2}{1}{3}{1}{4}{1}{5}{1}{6}{1}{7}{1}{8}{1}{9}",
-                                         "$200-299",
-                                         vbCrLf,
-                                         "$300-399",
-                                         "$400-499",
-                                         "$500-599",
-                                         "$600-699",
-                                         "$700-799",
-                                         "$800-899",
-                                         "$900-999",
-                                         "$999+")
+        ' Update labels with new results
+        UpdateLabels()
 
-        valuesLabel.Text = String.Format("{0}{1}{2}{1}{3}{1}{4}{1}{5}{1}{6}{1}{7}{1}{8}{1}{9}",
-                                 SalesRangeIntegerArray(0),
-                                 vbCrLf,
-                                 SalesRangeIntegerArray(1),
-                                 SalesRangeIntegerArray(2),
-                                 SalesRangeIntegerArray(3),
-                                 SalesRangeIntegerArray(4),
-                                 SalesRangeIntegerArray(5),
-                                 SalesRangeIntegerArray(6),
-                                 SalesRangeIntegerArray(7),
-                                 SalesRangeIntegerArray(8))
-
+        ' Clear and focus on data entry textbox
         With amountTextBox
             .Focus()
             .Clear()
@@ -72,21 +80,28 @@ Public Class SalarySurvey
     End Sub
 
     Private Sub SalarySurvey_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        rangesLabel.Text = String.Format("{0}{1}{2}{1}{3}{1}{4}{1}{5}{1}{6}{1}{7}{1}{8}{1}{9}",
-                                 "$200-299",
-                                 vbCrLf,
-                                 "$300-399",
-                                 "$400-499",
-                                 "$500-599",
-                                 "$600-699",
-                                 "$700-799",
-                                 "$800-899",
-                                 "$900-999",
-                                 "$999+")
+        ' Make sure that labels have been drawn before calculate button has been clicked
+        UpdateLabels()
+    End Sub
 
-        valuesLabel.Text = String.Format("{0}{1}{2}{1}{3}{1}{4}{1}{5}{1}{6}{1}{7}{1}{8}{1}{9}",
-                                 SalesRangeIntegerArray(0),
+    Private Sub UpdateLabels()
+        ' Format rangesLabel
+        rangesLabel.Text = String.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}{0}{8}{0}{9}",
+                         vbCrLf,
+                         "$200-299",
+                         "$300-399",
+                         "$400-499",
+                         "$500-599",
+                         "$600-699",
+                         "$700-799",
+                         "$800-899",
+                         "$900-999",
+                         "$999+")
+
+        ' Format valuesLabel
+        valuesLabel.Text = String.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}{0}{8}{0}{9}",
                                  vbCrLf,
+                                 SalesRangeIntegerArray(0),
                                  SalesRangeIntegerArray(1),
                                  SalesRangeIntegerArray(2),
                                  SalesRangeIntegerArray(3),
